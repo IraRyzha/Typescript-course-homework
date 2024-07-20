d3e9b15cb5108cb0a6d78c12bbe359f27db742b0
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-07-20 16:53:22 +0300

    ._.

--- index.ts
+++ index.ts
@@ -1,144 +1,42 @@
-abstract class Vehicle {
-  constructor(public brand: string) {}
-  abstract getType(): string;
-}
+// Task 1
 
-class Car extends Vehicle {
-  constructor(brand: string, public seats: number) {
-    super(brand);
-  }
-
-  getType(): string {
-    return "car";
-  }
-
-  performAction(): string {
-    return `Car ${this.brand} with ${this.seats} seats is starting its engine.`;
-  }
-}
-
-class Truck extends Vehicle {
-  constructor(brand: string, public capacity: number) {
-    super(brand);
-  }
-
-  getType(): string {
-    return "truck";
-  }
-
-  getAction(): string {
-    return `Truck ${this.brand} with capacity ${this.capacity} kg is loading cargo.`;
-  }
-}
-
-class Motorcycle extends Vehicle {
-  constructor(brand: string, public hasSidecar: boolean) {
-    super(brand);
-  }
-
-  getType(): string {
-    return "motorcycle";
-  }
-
-  action(): string {
-    const sidecarStatus = this.hasSidecar ? "with" : "without";
-    return `Motorcycle ${this.brand} ${sidecarStatus} sidecar is revving its engine.`;
-  }
-}
-
-class VehicleValidator {
-  private constructor() {}
-
-  public static isCar(vehicle: Vehicle): vehicle is Car {
-    return vehicle instanceof Car;
-  }
-
-  public static isTruck(vehicle: Vehicle): vehicle is Truck {
-    return vehicle instanceof Truck;
-  }
-
-  public static isMotorcycle(vehicle: Vehicle): vehicle is Motorcycle {
-    return vehicle instanceof Motorcycle;
-  }
-}
-
-class VehicleList {
-  private vehicles: Vehicle[] = [];
-
-  addVehicle(vehicle: Vehicle | Vehicle[]) {
-    if (Array.isArray(vehicle)) {
-      this.vehicles.push(...vehicle);
-    } else {
-      this.vehicles.push(vehicle);
-    }
-    // this.saveToLocalStorage();
-  }
-
-  performVehicleActions(number: number): any {
-    const selectedVehicle = this.vehicles[number];
-
-    if (VehicleValidator.isCar(selectedVehicle)) {
-      return selectedVehicle.performAction();
-    } else if (VehicleValidator.isTruck(selectedVehicle)) {
-      return selectedVehicle.getAction();
-    } else if (VehicleValidator.isMotorcycle(selectedVehicle)) {
-      return selectedVehicle.action();
-    } else {
-      throw new Error(`Unhandled vehicle type: ${selectedVehicle}`);
-    }
-  }
-
-  private saveToLocalStorage(): void {
-    const vehiclesJSON = this.vehicles.map((vehicle) => ({
-      type: vehicle.getType(),
-      ...vehicle,
-    }));
-    localStorage.setItem("vehicles", JSON.stringify(vehiclesJSON));
-  }
-
-  private loadFromLocalStorage(): void {
-    const vehiclesJSON = JSON.parse(localStorage.getItem("vehicles") || "[]");
-    console.log(vehiclesJSON);
-
-    this.vehicles = vehiclesJSON;
-  }
-
-  private vehicleFromJSON() /* ? */ {
-    // ?
-  }
-
-  public loadFromStorage() {
-    this.loadFromLocalStorage();
-    return this.vehicles;
-  }
-}
-
-const VehicleListObject = new VehicleList();
-const myCar = new Car("ferrari", 2);
-const myTruck = new Truck("niko trans", 12);
-const myMotorcycle = new Motorcycle("fire", true);
-
-const vehicleObjects = [myCar, myTruck, myMotorcycle];
-
-VehicleListObject.addVehicle(vehicleObjects);
-
-console.log(VehicleListObject.performVehicleActions(0));
-console.log(VehicleListObject.performVehicleActions(1));
-console.log(VehicleListObject.performVehicleActions(2));
-
-// const carTag: HTMLElement | null = document.querySelector(".CarInfo");
-// const truckTag: HTMLElement | null = document.querySelector(".TruckInfo");
-// const motorcycleTag: HTMLElement | null =
-//   document.querySelector(".MotorcycleInfo");
-
-// if (carTag) {
-//   carTag.innerHTML = `<p>${VehicleListObject.performVehicleActions(0)}</p>`;
+// function filterArray<T>(array: any[], condition: any) {
+//   return array.filter()
 // }
 
-// if (truckTag) {
-//   truckTag.textContent = VehicleListObject.performVehicleActions(1);
-// }
+// Task 2
 
-// if (motorcycleTag) {
-//   motorcycleTag.textContent = VehicleListObject.performVehicleActions(2);
-// }
+class Stack<T extends { id: number }> {
+  constructor(private items: T[] = []) {}
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): void {
+    this.items.pop();
+  }
+
+  peek(id: number): T | undefined {
+    return this.items.find((item) => item.id === id);
+  }
+}
+
+// Task 3
+
+interface IDictionary<V> {
+  [key: string]: V;
+}
+
+class Dictionary<K extends string, V> {
+  private items: any = {};
+  set(key: K, value: V) {
+    this.items[key] = value;
+  }
+  get(key: K): IDictionary<V> | undefined {
+    return this.items[key];
+  }
+  has(key: K) {
+    return this.items.hasOwnProperty(key) ? true : false;
+  }
+}
