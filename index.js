d00580e0c1df6258041c4a9ca20fb6554f4b364e
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-07-20 16:29:01 +0300

    ._>

--- index.js
+++ index.js
@@ -1,66 +1,161 @@
-var user = {
-    name: "Tom",
-    age: 11,
-};
-var example = {
-    sum: function (a, b) { return a + b; },
-    say: function (message) { return console.log(message); },
-    queNumber: function (num) { return console.log(num); },
-};
-example.queNumber(true);
-var exampleArray1 = [3, 5, "Hello", 7, "hi", 1];
-var exampleArray2 = {
-    0: true,
-    1: false,
-    2: true,
-};
-var myDog = {
-    name: "Cabaka",
-    age: 3,
-};
-var studend = {
-    name: "randomName",
-    age: 25,
-    job: "dev",
-    marks: [5, 4, 1, 3, 7],
-    address: {
-        index: 1,
-        country: "uk",
-        city: "vin",
-    },
-};
-function isObject(value) {
-    return typeof value === "object" && value !== null && !Array.isArray(value);
-}
-function isNumber(value) {
-    if (Array.isArray(value)) {
-        for (var _i = 0, value_1 = value; _i < value_1.length; _i++) {
-            var el = value_1[_i];
-            if (typeof el !== "number") {
-                return false;
-            }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
-        return true;
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var Vehicle = /** @class */ (function () {
+    function Vehicle(brand) {
+        this.brand = brand;
     }
-    if (isObject(value)) {
-        for (var el in value) {
-            if (typeof value[el] !== "number") {
-                return false;
-            }
-        }
-        return true;
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car(brand, seats) {
+        var _this = _super.call(this, brand) || this;
+        _this.seats = seats;
+        return _this;
     }
-    return typeof value === "number";
-}
-function checkFieldIsNumberType(object) {
-    for (var key in object) {
-        if (key === "name" || key === "job" || key === "country") {
-            continue;
-        }
-        if (!isNumber(object[key])) {
-            return false;
-        }
+    Car.prototype.getType = function () {
+        return "car";
+    };
+    Car.prototype.performAction = function () {
+        return "Car ".concat(this.brand, " with ").concat(this.seats, " seats is starting its engine.");
+    };
+    return Car;
+}(Vehicle));
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck(brand, capacity) {
+        var _this = _super.call(this, brand) || this;
+        _this.capacity = capacity;
+        return _this;
     }
-    return true;
-}
-// console.log(checkFieldIsNumberType(studend));
+    Truck.prototype.getType = function () {
+        return "truck";
+    };
+    Truck.prototype.getAction = function () {
+        return "Truck ".concat(this.brand, " with capacity ").concat(this.capacity, " kg is loading cargo.");
+    };
+    return Truck;
+}(Vehicle));
+var Motorcycle = /** @class */ (function (_super) {
+    __extends(Motorcycle, _super);
+    function Motorcycle(brand, hasSidecar) {
+        var _this = _super.call(this, brand) || this;
+        _this.hasSidecar = hasSidecar;
+        return _this;
+    }
+    Motorcycle.prototype.getType = function () {
+        return "motorcycle";
+    };
+    Motorcycle.prototype.action = function () {
+        var sidecarStatus = this.hasSidecar ? "with" : "without";
+        return "Motorcycle ".concat(this.brand, " ").concat(sidecarStatus, " sidecar is revving its engine.");
+    };
+    return Motorcycle;
+}(Vehicle));
+var VehicleValidator = /** @class */ (function () {
+    function VehicleValidator() {
+    }
+    VehicleValidator.isCar = function (vehicle) {
+        return vehicle instanceof Car;
+    };
+    VehicleValidator.isTruck = function (vehicle) {
+        return vehicle instanceof Truck;
+    };
+    VehicleValidator.isMotorcycle = function (vehicle) {
+        return vehicle instanceof Motorcycle;
+    };
+    return VehicleValidator;
+}());
+var VehicleList = /** @class */ (function () {
+    function VehicleList() {
+        this.vehicles = [];
+    }
+    VehicleList.prototype.addVehicle = function (vehicle) {
+        var _a;
+        if (Array.isArray(vehicle)) {
+            (_a = this.vehicles).push.apply(_a, vehicle);
+        }
+        else {
+            this.vehicles.push(vehicle);
+        }
+        // this.saveToLocalStorage();
+    };
+    VehicleList.prototype.performVehicleActions = function (number) {
+        var selectedVehicle = this.vehicles[number];
+        if (VehicleValidator.isCar(selectedVehicle)) {
+            return selectedVehicle.performAction();
+        }
+        else if (VehicleValidator.isTruck(selectedVehicle)) {
+            return selectedVehicle.getAction();
+        }
+        else if (VehicleValidator.isMotorcycle(selectedVehicle)) {
+            return selectedVehicle.action();
+        }
+        else {
+            throw new Error("Unhandled vehicle type: ".concat(selectedVehicle));
+        }
+    };
+    VehicleList.prototype.saveToLocalStorage = function () {
+        var vehiclesJSON = this.vehicles.map(function (vehicle) { return (__assign({ type: vehicle.getType() }, vehicle)); });
+        localStorage.setItem("vehicles", JSON.stringify(vehiclesJSON));
+    };
+    VehicleList.prototype.loadFromLocalStorage = function () {
+        var vehiclesJSON = JSON.parse(localStorage.getItem("vehicles") || "[]");
+        console.log(vehiclesJSON);
+        this.vehicles = vehiclesJSON;
+    };
+    VehicleList.prototype.vehicleFromJSON = function () {
+        // ?
+    };
+    VehicleList.prototype.loadFromStorage = function () {
+        this.loadFromLocalStorage();
+        return this.vehicles;
+    };
+    return VehicleList;
+}());
+var VehicleListObject = new VehicleList();
+var myCar = new Car("ferrari", 2);
+var myTruck = new Truck("niko trans", 12);
+var myMotorcycle = new Motorcycle("fire", true);
+var vehicleObjects = [myCar, myTruck, myMotorcycle];
+VehicleListObject.addVehicle(vehicleObjects);
+console.log(VehicleListObject.performVehicleActions(0));
+console.log(VehicleListObject.performVehicleActions(1));
+console.log(VehicleListObject.performVehicleActions(2));
+// const carTag: HTMLElement | null = document.querySelector(".CarInfo");
+// const truckTag: HTMLElement | null = document.querySelector(".TruckInfo");
+// const motorcycleTag: HTMLElement | null =
+//   document.querySelector(".MotorcycleInfo");
+// if (carTag) {
+//   carTag.innerHTML = `<p>${VehicleListObject.performVehicleActions(0)}</p>`;
+// }
+// if (truckTag) {
+//   truckTag.textContent = VehicleListObject.performVehicleActions(1);
+// }
+// if (motorcycleTag) {
+//   motorcycleTag.textContent = VehicleListObject.performVehicleActions(2);
+// }
--- index.ts
+++ index.ts
@@ -1,108 +1,144 @@
-// Task 1
-interface RandomIndex {
-  [key: string]: string | number;
+abstract class Vehicle {
+  constructor(public brand: string) {}
+  abstract getType(): string;
 }
 
-const user: RandomIndex = {
-  name: "Tom",
-  age: 11,
-};
-
-// Task 2
-interface FunctionIndex {
-  [key: string]: (...params: any[]) => any;
-}
-
-const example: FunctionIndex = {
-  sum: (a: number, b: number) => a + b,
-  say: (message: string) => console.log(message),
-};
-
-// Task 3
-interface ArrayIndex<T> {
-  [key: number]: T;
-}
-
-const exampleArray1: ArrayIndex<number | string> = [3, 5, "Hello", 7, "hi", 1];
-const exampleArray2: ArrayIndex<boolean> = {
-  0: true,
-  1: false,
-  2: true,
-};
-
-// Task 4
-interface Identifier {
-  name: string;
-  [key: string]: string | number;
-}
-
-const myDog: Identifier = {
-  name: "Cabaka",
-  age: 3,
-};
-
-// Task 5
-interface IdentifierSuper {
-  [key: string]: string | number | boolean | (() => void);
-}
-
-interface IdentifierSub extends IdentifierSuper {
-  id: number;
-  isOwner: boolean;
-  run(): () => void;
-  stop(): () => void;
-}
-
-// Task 6
-interface IIdentifier {
-  [key: string]: any;
-}
-
-const studend: IIdentifier = {
-  name: "randomName",
-  age: 25,
-  job: "dev",
-  marks: [5, 4, 1, 3, 7],
-  address: {
-    index: 1,
-    country: "uk",
-    city: "vin",
-  },
-};
-
-function isObject(value: any): boolean {
-  return typeof value === "object" && value !== null && !Array.isArray(value);
-}
-
-function isNumber(value: any): boolean {
-  if (Array.isArray(value)) {
-    for (let el of value) {
-      if (typeof el !== "number") {
-        return false;
-      }
-    }
-    return true;
+class Car extends Vehicle {
+  constructor(brand: string, public seats: number) {
+    super(brand);
   }
-  if (isObject(value)) {
-    for (let el in value) {
-      if (typeof value[el] !== "number") {
-        return false;
-      }
-    }
-    return true;
+
+  getType(): string {
+    return "car";
   }
-  return typeof value === "number";
+
+  performAction(): string {
+    return `Car ${this.brand} with ${this.seats} seats is starting its engine.`;
+  }
 }
 
-function checkFieldIsNumberType(object: IIdentifier) {
-  for (const key in object) {
-    if (key === "name" || key === "job" || key === "country") {
-      continue;
+class Truck extends Vehicle {
+  constructor(brand: string, public capacity: number) {
+    super(brand);
+  }
+
+  getType(): string {
+    return "truck";
+  }
+
+  getAction(): string {
+    return `Truck ${this.brand} with capacity ${this.capacity} kg is loading cargo.`;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(brand: string, public hasSidecar: boolean) {
+    super(brand);
+  }
+
+  getType(): string {
+    return "motorcycle";
+  }
+
+  action(): string {
+    const sidecarStatus = this.hasSidecar ? "with" : "without";
+    return `Motorcycle ${this.brand} ${sidecarStatus} sidecar is revving its engine.`;
+  }
+}
+
+class VehicleValidator {
+  private constructor() {}
+
+  public static isCar(vehicle: Vehicle): vehicle is Car {
+    return vehicle instanceof Car;
+  }
+
+  public static isTruck(vehicle: Vehicle): vehicle is Truck {
+    return vehicle instanceof Truck;
+  }
+
+  public static isMotorcycle(vehicle: Vehicle): vehicle is Motorcycle {
+    return vehicle instanceof Motorcycle;
+  }
+}
+
+class VehicleList {
+  private vehicles: Vehicle[] = [];
+
+  addVehicle(vehicle: Vehicle | Vehicle[]) {
+    if (Array.isArray(vehicle)) {
+      this.vehicles.push(...vehicle);
+    } else {
+      this.vehicles.push(vehicle);
     }
-    if (!isNumber(object[key])) {
-      return false;
+    // this.saveToLocalStorage();
+  }
+
+  performVehicleActions(number: number): any {
+    const selectedVehicle = this.vehicles[number];
+
+    if (VehicleValidator.isCar(selectedVehicle)) {
+      return selectedVehicle.performAction();
+    } else if (VehicleValidator.isTruck(selectedVehicle)) {
+      return selectedVehicle.getAction();
+    } else if (VehicleValidator.isMotorcycle(selectedVehicle)) {
+      return selectedVehicle.action();
+    } else {
+      throw new Error(`Unhandled vehicle type: ${selectedVehicle}`);
     }
   }
-  return true;
+
+  private saveToLocalStorage(): void {
+    const vehiclesJSON = this.vehicles.map((vehicle) => ({
+      type: vehicle.getType(),
+      ...vehicle,
+    }));
+    localStorage.setItem("vehicles", JSON.stringify(vehiclesJSON));
+  }
+
+  private loadFromLocalStorage(): void {
+    const vehiclesJSON = JSON.parse(localStorage.getItem("vehicles") || "[]");
+    console.log(vehiclesJSON);
+
+    this.vehicles = vehiclesJSON;
+  }
+
+  private vehicleFromJSON() /* ? */ {
+    // ?
+  }
+
+  public loadFromStorage() {
+    this.loadFromLocalStorage();
+    return this.vehicles;
+  }
 }
-// console.log(checkFieldIsNumberType(studend));
+
+const VehicleListObject = new VehicleList();
+const myCar = new Car("ferrari", 2);
+const myTruck = new Truck("niko trans", 12);
+const myMotorcycle = new Motorcycle("fire", true);
+
+const vehicleObjects = [myCar, myTruck, myMotorcycle];
+
+VehicleListObject.addVehicle(vehicleObjects);
+
+console.log(VehicleListObject.performVehicleActions(0));
+console.log(VehicleListObject.performVehicleActions(1));
+console.log(VehicleListObject.performVehicleActions(2));
+
+// const carTag: HTMLElement | null = document.querySelector(".CarInfo");
+// const truckTag: HTMLElement | null = document.querySelector(".TruckInfo");
+// const motorcycleTag: HTMLElement | null =
+//   document.querySelector(".MotorcycleInfo");
+
+// if (carTag) {
+//   carTag.innerHTML = `<p>${VehicleListObject.performVehicleActions(0)}</p>`;
+// }
+
+// if (truckTag) {
+//   truckTag.textContent = VehicleListObject.performVehicleActions(1);
+// }
+
+// if (motorcycleTag) {
+//   motorcycleTag.textContent = VehicleListObject.performVehicleActions(2);
+// }
